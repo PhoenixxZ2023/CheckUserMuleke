cd4efecaa8192a0c618eec088845966c362bae59
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-01-12 13:42:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ azul="\e[34m"
 roxo="\e[38;2;128;0;128m"
 reset="\e[0m"
 
-rm -rf /root/ApiUlekCheckuser/
-rm -f /usr/local/bin/ulekCheckuser
-pkill -9 -f "/root/ApiUlekCheckuser/api.py"
+rm -rf /root/CheckUserMuleke/
+rm -f /usr/local/bin/MulekeCheckuser
+pkill -9 -f "/root/CheckUserMuleke/api.py"
 
 apt update && apt upgrade -y && apt install python3 git -y
-git clone https://github.com/UlekBR/ApiUlekCheckuser.git
-chmod +x /root/ApiUlekCheckuser/apiMenu.sh
-ln -s /root/ApiUlekCheckuser/apiMenu.sh /usr/local/bin/apiUlekCheckuser
+git clone https://github.com/PhoenixxZ2023/CheckUserMuleke.git
+chmod +x /root/CheckUserMuleke/apiMenu.sh
+ln -s /root/CheckUserMuleke/apiMenu.sh /usr/local/bin/apiUlekCheckuser
 
 clear
-echo -e "Para iniciar o menu digite: ${verde}apiUlekCheckuser${reset}"
+echo -e "Para iniciar o menu digite: ${verde}CheckUserMuleke${reset}"
